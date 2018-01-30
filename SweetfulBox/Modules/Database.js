e55b4eb334674a660ded14eab8ceabba15060083
ace68723 <ace68723@gmail.com>
2018-01-30 11:09:44 -0500

    databse sbox_addItemToCart

--- SweetfulBox/Modules/Database.js
+++ SweetfulBox/Modules/Database.js
@@ -9,13 +9,13 @@ const sbox_cart_scheam = {
   properties: {
    sku_id:'int',
    spu_id:'int',
+   sku_status:'int',
    spu_name:"string",
    sku_name:"string",
-   sku_status:'int',
    sku_amount:'int',
+   sku_quantity:'int',
    sku_original_price:'string',
    sku_price:'string',
-   sku_quantity:'int',
    sku_image_url:'string'
   }
 }
@@ -37,4 +37,32 @@ export function sbox_getAllItemsFromCart() {
 }
 export function sbox_addItemsQuantityFromCart(sku_id) {
   return realm_sbox.objects('sbox_cart');
+}
+export function sbox_addItemToCart() {
+  const sku_id = io_data.sku_id
+  const spu_id = io_data.spu_id
+  const sku_status = io_data.sku_status
+  const spu_name = io_data.spu_name
+  const sku_name = io_data.sku_name
+  const sku_amount = io_data.sku_amount
+  const sku_quantity = io_data.sku_quantity
+  const sku_original_price = io_data.sku_original_price
+  const sku_price = io_data.sku_price
+  const sku_image_url = io_data.sku_image_url
+  const data ={
+    sku_id,
+    spu_id,
+    sku_status,
+    spu_name,
+    sku_name,
+    sku_amount,
+    sku_quantity,
+    sku_original_price,
+    sku_price,
+    sku_image_url
+  }
+  realm.write(() => {
+    realm.create('sbox_cart_product',Object.assign({},data),true);
+  })
+
 }
