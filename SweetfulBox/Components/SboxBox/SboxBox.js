06c3f70d524d08f81dfb44c73abfc6cc46b6b5b3
ShuWangSam <shu.wang@hotmail.ca>
2018-01-30 11:02:14 -0500

    sboxbox fixed

--- SweetfulBox/Components/SboxBox/SboxBox.js
+++ SweetfulBox/Components/SboxBox/SboxBox.js
@@ -22,9 +22,7 @@ export default class MyComponent extends Component {
     super();
     this.state = {
       currentBox:{boxWeights:0},
-      boxLeft: new Animated.Value(0),
-      showClaw:'down',
-      clawPosition: new Animated.Value((-height-200)*2),
+      boxLeft: new Animated.Value(width*0.8),
       boxPosition: new Animated.Value(0),
     }
     this._onRealmChange = this._onRealmChange.bind(this);
@@ -32,9 +30,6 @@ export default class MyComponent extends Component {
   componentDidMount() {
     realm.addListener('change', this._onRealmChange)
     this._onRealmChange();
-    // setTimeout(() => {
-    //   this._updateBoxPosition();
-    // }, 600);
   }
   componentWillUnmount() {
     realm.removeListener('change',this._onRealmChange);
@@ -51,118 +46,8 @@ export default class MyComponent extends Component {
       this._updateBoxPosition();
     }, 100);
   }
-  _updateBoxPosition() {
-    console.log('currentBox',(this.state.boxId!==1 && this._boxId !== this.state.currentBox.boxId))
 
-    // if (this.state.currentBox.boxWeights === 99 || (this.state.boxId!==1 && this._boxId !== this.state.currentBox.boxId)){
-      if (this.state.currentBox.boxWeights === 99){
-      this._boxId = this.state.boxId;
-      const boxPosition = 99 / 99 * width-80;
-      Animated.timing(this.state.boxLeft, {
-        toValue: boxPosition,
-        duration: 200,
-        useNativeDriver: true,
-      }).start();
-      Animated.timing(this.state.clawPosition, {
-        toValue: -height-200,
-        delay:1000,
-        duration: 500,
-        useNativeDriver: true,
-      }).start();
-      setTimeout(() => {
-        this.setState({
-          showClaw:'up',
-        })
-        Animated.timing(this.state.clawPosition, {
-          toValue: (-height-200)*2,
-          duration: 500,
-          useNativeDriver: true,
-        }).start();
-        Animated.timing(this.state.boxPosition, {
-          toValue: -height+80,
-          duration: 400,
-          useNativeDriver: true,
-        }).start();
-      }, 2500);
-
-    } else {
-      const boxPosition = this.state.currentBox.boxWeights / 99 * (width - 80);
-      // const boxPosition = 99 / 99 * width-80;
-      Animated.timing(this.state.boxLeft, {
-        toValue: boxPosition,
-        duration: 200,
-        useNativeDriver: true,
-      }).start();
-    }
-  }
-  _renderClaw() {
-    if(this.state.showClaw == 'down'){
-      return(
-        <Animated.Image
-              style={{
-                width:100,
-                height:100*8.4357,
-                left:-24,
-                position:'absolute',
-                transform:[
-                  {translateX: this.state.boxLeft},
-                  {translateY: this.state.clawPosition}
-                ]
-              }}
-              source={require('./Image/claw01.png')}>
-        </Animated.Image>
-      )
-    }else{
-      return(
-        <Animated.Image
-              style={{
-                width:100,
-                height:100*8.4357,
-                left:-24,
-                position:'absolute',
-                transform:[
-                  {translateX: this.state.boxLeft},
-                  {translateY: this.state.clawPosition}
-                ]
-              }}
-              source={require('./Image/claw02.png')}>
-        </Animated.Image>
-      )
-    }
-  }
   _renderBox() {
-    if(this.state.showClaw == 'down'){
-      return(
-        <View style={{
-                  height:20,
-                  backgroundColor:'#ff768b',
-                  borderColor:'#000000',
-                  borderTopWidth:3,
-                  position:'absolute',
-                  bottom:0,
-                  width:width,
-                }}>
-              <TouchableOpacity onPress={this.props.goToSboxCart}>
-                <AnimatedImageBackground
-                      style={{
-                        width:50,
-                        height:50,
-                        top:-42,
-                        alignItems:'center',
-                        justifyContent:'center',
-                        transform:[
-                          {translateX: this.state.boxLeft},
-                        ]
-                      }}
-                      source={require('./Image/box.png')}>
-                  <Text style={{backgroundColor:'rgba(0,0,0,0)',fontFamily:'FZZhunYuan-M02S',}}>
-                     {this.state.currentBox.boxWeights}/99
-                  </Text>
-                </AnimatedImageBackground>
-              </TouchableOpacity>
-        </View>
-      )
-    }else{
       return(
         <View style={{
                   height:20,
@@ -194,15 +79,11 @@ export default class MyComponent extends Component {
           </TouchableOpacity>
         </View>
       )
-    }
   }
   render() {
     return (
       <View >
-
           {this._renderBox()}
-
-        {this._renderClaw()}
       </View>
     );
   }
