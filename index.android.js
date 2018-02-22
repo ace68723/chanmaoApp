b23fb7925cadc9e893748e668b2d6d14e40c9071
ricardol01 <mgu@yorku.ca>
2018-02-22 14:35:08 -0500

    merge

--- index.android.js
+++ index.android.js
@@ -1,14 +1,19 @@
 import { DatabaseInit } from './App/Modules/Database';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
+
+import codePush from "react-native-code-push";
+
 DatabaseInit();
 registerScreens();
+
+codePush.sync({ installMode: codePush.InstallMode.IMMEDIATE});
 Navigation.startSingleScreenApp({
   screen: {
     // screen: 'cmHome', CmEat CmLogin// unique ID registered with Navigation.registerScreen
     screen: 'cmHome',
-    navigatorStyle: {navBarHidden: true}, 
-    navigatorButtons: {}, 
+    navigatorStyle: {navBarHidden: true},
+    navigatorButtons: {},
 
   },
   animated: true,
