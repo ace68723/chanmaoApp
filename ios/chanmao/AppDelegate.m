3eea867ea90e49fa11c542511a529dfe6b3814e0
ace68723 <ace68723@gmail.com>
2018-05-28 23:28:50 -0400

    2.6.0-2.6.1 release

--- ios/chanmao/AppDelegate.m
+++ ios/chanmao/AppDelegate.m
@@ -31,7 +31,7 @@
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [CodePush bundleURL];
 #endif
   
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
