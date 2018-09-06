1add0457ba2a5de1525fb988477584f67f244c2c
Franky Chen <Franky@Franky-2.hitronhub.home>
2018-09-06 11:02:04 -0400

    Fixed syntax typo when get address detail by placeid

--- CmEat/Actions/AddressAction.js
+++ CmEat/Actions/AddressAction.js
@@ -21,76 +21,84 @@ export default {
     }
   },
   async getPlaceIdFromAddress(address) {
-    const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/" +
-      "json?input=" + address + "&inputtype=textquery" +
-      "&key=" + AppConstants.GOOGLE_API_KEY
-    return fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.status == 'OK' && res.candidates.length > 0) {
-          return res.candidates[0].place_id
-        }
-      })
-      .catch((error) => {
-        throw error
-      })
+    try {
+      const url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/" +
+        "json?input=" + address + "&inputtype=textquery" +
+        "&key=" + AppConstants.GOOGLE_API_KEY
+      return fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.status == 'OK' && res.candidates.length > 0) {
+            return res.candidates[0].place_id
+          }
+        })
+        .catch((error) => {
+          throw error
+        })
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   async formatAddress(description) {
-    const placeId = await this.getPlaceIdFromAddress(description);
-    const url = "https://maps.googleapis.com/maps/api/place/details/" +
-      "json?placeid=" + placeId +
-      "&key=" + AppConstants.GOOGLE_API_KEY
-    let options = {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.status == "OK") {
-          const placeDetails = res.result;
-
-          let addrInfo = {};
-          addrInfo.latitude = placeDetails.geometry.location.lat;
-          addrInfo.longitude = placeDetails.geometry.location.lng;
-          _forEach(placeDetails.address_components, function(component, key) {
-            _forEach(component.types, function(type, key) {
-              switch (type) {
-                case "postal_code":
-                  addrInfo.postalCode = component.long_name;
-                  break;
-                case "locality":
-                  addrInfo.city = component.long_name;
-                  break;
-                case "sublocality":
-                  addrInfo.city = component.long_name;
-                  break;
-                case "neighborhood":
-                  addrInfo.city = component.long_name;
-                  break;
-              }
-            });
-          });
-          if (!addrInfo.city) {
-            addrInfo.city = 'GTA';
-          }
-          addrInfo.address = placeDetails.formatted_address
-          dispatch({
-            actionType: AppConstants.FORMAT_ADDRESS,
-            addrInfo
-          })
-        } else {
-          throw 'error'
+    try {
+      const placeId = await this.getPlaceIdFromAddress(description);
+      const url = "https://maps.googleapis.com/maps/api/place/details/" +
+        "json?placeid=" + placeId +
+        "&key=" + AppConstants.GOOGLE_API_KEY
+      let options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
         }
-      })
-      .catch((error) => {
-        throw error
-      })
+      }
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.status == "OK") {
+            const placeDetails = res.result;
+
+            let addrInfo = {};
+            addrInfo.latitude = placeDetails.geometry.location.lat;
+            addrInfo.longitude = placeDetails.geometry.location.lng;
+            _forEach(placeDetails.address_components, function(component, key) {
+              _forEach(component.types, function(type, key) {
+                switch (type) {
+                  case "postal_code":
+                    addrInfo.postalCode = component.long_name;
+                    break;
+                  case "locality":
+                    addrInfo.city = component.long_name;
+                    break;
+                  case "sublocality":
+                    addrInfo.city = component.long_name;
+                    break;
+                  case "neighborhood":
+                    addrInfo.city = component.long_name;
+                    break;
+                }
+              });
+            });
+            if (!addrInfo.city) {
+              addrInfo.city = 'GTA';
+            }
+            addrInfo.address = placeDetails.formatted_address
+            dispatch({
+              actionType: AppConstants.FORMAT_ADDRESS,
+              addrInfo
+            })
+          } else {
+            throw 'error'
+          }
+        })
+        .catch((error) => {
+          throw error
+        })
+    } catch (error) {
+      console.log(error);
+    }
   },
   async submitAddress(userInfo) {
     try {
