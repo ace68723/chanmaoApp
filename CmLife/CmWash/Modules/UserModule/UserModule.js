e2f739fb172271601c7a4cc90de7b9a06b1dd0ac
Khalil <khalilleo@users.noreply.github.com>
2018-12-12 16:14:07 -0500

    [CMWash] Added unit number and buzz number to address info

--- CmLife/CmWash/Modules/UserModule/UserModule.js
+++ CmLife/CmWash/Modules/UserModule/UserModule.js
@@ -6,14 +6,21 @@ export default  {
   async putUserAddr(io_data){
     const {uid,token,version} = GetUserInfo();
     try {
+      let addressString = io_data.addressObject.addr;
+      if (io_data.unitNumber && io_data.unitNumber.length > 0){
+        addressString = `Unit ${io_data.unitNumber}, ${addressString}`;
+      }
+      if (io_data.buzz && io_data.buzz.length > 0){
+        addressString = `Buzz ${io_data.buzz}, ${addressString}`;
+      }
+      console.log(addressString);
       const lo_data ={
         authortoken: token,
-        iv_addr: io_data.addressObject.addr,
+        iv_addr: addressString,
         iv_province: io_data.addressObject.province,
         iv_lat: io_data.addressObject.lat.toString(),
         iv_lng: io_data.addressObject.lng.toString(),
         iv_name: io_data.name,
-        // iv_unit: io_data.unitNumber,
         iv_phone:  io_data.phoneNumber,
       }
       const result = await UserAPI.putUserAddr(lo_data);
