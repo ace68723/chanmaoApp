0957662ea40c34c233d5649dddff4e40b72d863c
ricardol01 <mgu@yorku.ca>
2019-02-22 11:10:00 -0500

    enable codepush

--- App/Components/CodePushUpdate/CodePushUpdate.js
+++ App/Components/CodePushUpdate/CodePushUpdate.js
@@ -27,7 +27,7 @@ export default class CodePushUpdate extends Component {
         CodePush.allowRestart() //允许重启，否则热更新不会生效
         CodePush.sync({
             checkFrequency: CodePush.CheckFrequency.ON_APP_START,
-            installMode: CodePush.InstallMode.ON_NEXT_RESTART
+            installMode: CodePush.InstallMode.IMMEDIATE
           },
             (status)=>{
               switch(status) {
