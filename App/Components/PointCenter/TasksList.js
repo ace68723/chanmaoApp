9451df309e7c8fc5c91a103ffc4ef078b4ce55ab
Khalil <khalilleo@users.noreply.github.com>
2019-03-13 13:08:18 -0400

    View prep

---
+++ App/Components/PointCenter/TasksList.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {
+  Animated,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList
+} from "react-native";
+
+import { GetUserInfo } from "../../Modules/Database";
+import moment from "moment";
+
+const { height, width } = Dimensions.get("window");
+
+class TasksList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3f3f3"
+  },
+  cellStyle: {
+    height: 48
+  }
+});
+
+module.exports = TasksList;
---
+++ App/Components/PointCenter/index.js
@@ -0,0 +1,41 @@
+import React, {
+    Component,
+} from 'react';
+import {
+    Animated,
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    FlatList
+} from 'react-native';
+
+import { GetUserInfo } from '../../Modules/Database';
+import moment from 'moment'
+
+const { height, width } = Dimensions.get('window');
+
+class PointCenter extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <View style={styles.container} >
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f3f3f3',
+    },
+    cellStyle: {
+        height: 48,
+    }
+});
+
+module.exports = PointCenter;
